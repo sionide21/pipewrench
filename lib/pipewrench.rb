63eae92574f30e371728112aaf2486f30e424574
Ben Olive <ben.olive@salesloft.com>
2015-12-11 21:17:13 -0500

    Add options for map, compact, and strip

--- lib/pipewrench.rb
+++ lib/pipewrench.rb
@@ -1,18 +1,35 @@
 require "pipewrench/version"
+require "pipewrench/options"
 
-module Pipewrench
-  class Shell
-    include Enumerable
+class Pipewrench
+  attr_reader :options
+  include Enumerable
 
-    def initialize(stdin)
-      @stdin = stdin
+  def initialize(stdin, options=Options.new)
+    @stdin = stdin
+    @options = options
+  end
+
+  def run(cmd)
+    if options.compact
+      evaluate(cmd).compact
+    else
+      evaluate(cmd)
     end
+  end
 
-    def run(cmd)
-      instance_eval(cmd)
+  def evaluate(cmd)
+    if options.map
+      map { |line| line.instance_eval(cmd) }
+    else
+      to_a.instance_eval(cmd)
     end
+  end
 
-    def each(&blk)
+  def each(&blk)
+    if options.strip
+      @stdin.each_line.map(&:rstrip).map(&blk)
+    else
       @stdin.each_line(&blk)
     end
   end
---
+++ lib/pipewrench/options.rb
@@ -0,0 +1,54 @@
+require "optparse"
+require "pipewrench/version"
+
+class Pipewrench
+  class Options
+    attr_reader :map, :strip, :compact
+
+    def parse!(*args)
+      parser.parse!(*args)
+      self
+    rescue OptionParser::InvalidOption => e
+      puts e
+      help
+    end
+
+    def to_s
+      parser.to_s
+    end
+
+    def help
+      puts self
+      exit
+    end
+
+    private
+
+    def parser
+      @parser ||= OptionParser.new do |opts|
+        opts.banner = "Usage: pipewrench [options] expression"
+
+        opts.on("-c", "--compact", "Remove nil lines from output") do |m|
+          @compact = true
+        end
+
+        opts.on("-m", "--map", "Run each line through the expression") do |m|
+          @map = true
+        end
+
+        opts.on("-s", "--strip", "Strip trailing whitespace from each line before running") do |m|
+          @strip = true
+        end
+
+        opts.on_tail("-h", "--help", "Show this message") do
+          help
+        end
+
+        opts.on_tail("--version", "Show version") do
+          puts "Pipewrench #{Pipewrench::VERSION}"
+          exit
+        end
+      end
+    end
+  end
+end
